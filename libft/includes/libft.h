0037ca6b988fa02602af373cecb7ea5c271e015f
MaN1x <mister.kozlov.maxim@yandex.ru>
2020-07-27 20:46:26 +0300

    added ft_atoi_hex()

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -86,5 +86,6 @@ t_list				*ft_lst_n(t_list *lst, size_t n);
 void				ft_lstaddn(t_list **lst, t_list *new, size_t n);
 void				ft_lstdelh(t_list **lst, void(*del)(void*, size_t));
 int					ft_isspace(int c);
+int					ft_atoi_hex(const char *str);
 
 #endif
